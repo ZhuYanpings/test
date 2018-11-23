62d6b18e5186e0a9e5614daacf43594f545a4f35
forname <1515108997@qq.com>
2018-11-23 13:43:23 +0800

    测试

---
+++ src/main/java/com/tecode/SpringSshApplication.java
@@ -0,0 +1,12 @@
+package com.tecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSshApplication.class, args);
+	}
+}
